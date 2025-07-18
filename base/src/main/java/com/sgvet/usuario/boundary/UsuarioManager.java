36eee394c1cf689b72b754b901aa6247b0863e50
cbustamante <carlos.bustamante@siriusxm.com>
2025-07-18 20:42:52 -0300

    release w. ignore

--- base/src/main/java/com/sgvet/usuario/boundary/UsuarioManager.java
+++ base/src/main/java/com/sgvet/usuario/boundary/UsuarioManager.java
@@ -1,17 +1,19 @@
-package com.sgvet.base.boundary;
+package com.sgvet.usuario.boundary;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+
+import com.sgvet.base.boundary.BaseDbManager;
 public class UsuarioManager {
     public static void main(String[] args) {
         // Esto inicializa automáticamente la base al levantar el singleton
-        Connection conn = DbManager.getInstance().getConnection();
+        Connection conn = BaseDbManager.getInstance().getConnection();
 
         try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT * FROM PERSONA")) {
+             ResultSet rs = stmt.executeQuery("SELECT * FROM USUARIO")) {
 
-            System.out.println("Contenido de la tabla PERSONA:");
+            System.out.println("Contenido de la tabla USUARIO:");
             while (rs.next()) {
                 System.out.printf("ID: %d, Nombre: %s%n", rs.getInt("ID"), rs.getString("NOMBRE"));
             }
